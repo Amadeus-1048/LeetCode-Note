f92a29453d2c1e605988ee7ea0c26e884a1a5d43
amadeus <amadeus1048@outlook.com>
2023-09-15 18:54:42 +0800

    feat: add new notes

--- code/functions/dp.go
+++ code/functions/dp.go
@@ -249,3 +249,23 @@ func numSquares(n int) int {
 	}
 	return dp[n]
 }
+
+// 198. 打家劫舍
+func rob(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return nums[0]
+	}
+	if n == 2 {
+		return max(nums[0], nums[1])
+	}
+	dp := make([]int, n)
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < n; i++ {
+		dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+	}
+	return dp[n-1]
+}
+
+// 213. 打家劫舍 II
