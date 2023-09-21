d7dc04ecb19cf83eca6028ebd531003358e85e1d
amadeus <amadeus1048@outlook.com>
2023-09-21 23:47:20 +0800

    feat: add new notes

---
+++ code/functions/graph_test.go
@@ -0,0 +1,20 @@
+package functions
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDijkstra(t *testing.T) {
+	// 带权值邻接矩阵
+	var gp = [][]int{
+		{0, 100, 1200, INF, INF, INF},
+		{100, 0, 900, 300, INF, INF},
+		{1200, 900, 0, 400, 500, INF},
+		{INF, 300, 400, 0, 1300, 1400},
+		{INF, INF, 500, 1300, 0, 1500},
+		{INF, INF, INF, 1400, 1500, 0},
+	}
+	dist := dijkstra(gp, 0)
+	fmt.Println(dist)
+}
