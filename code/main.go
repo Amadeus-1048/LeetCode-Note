0eaaa79723af1de6e1b7d11068fb854528f4292d
amadeus <amadeus1048@outlook.com>
2024-02-12 18:04:52 +0800

    feat: add new notes

--- code/main.go
+++ code/main.go
@@ -63,6 +63,22 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func getMostGold(nodes *TreeNode) {
-
+func mySqrt(x int) int {
+	res := 0 // x 平方根的整数部分 ans 是满足 k^2 ≤x 的最大 k 值
+	sqrt := 0
+	left, right := 0, x
+	if x <= 1 {
+		return x
+	} else {
+		for left <= right { // 对 k 进行二分查找
+			sqrt = left + (right-left)/2
+			if sqrt*sqrt <= x { // 比较中间元素的平方与 x 的大小关系
+				res = sqrt
+				left = sqrt + 1
+			} else {
+				right = sqrt - 1
+			}
+		}
+	}
+	return res
 }
